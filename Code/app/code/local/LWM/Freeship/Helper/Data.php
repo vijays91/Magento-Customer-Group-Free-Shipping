6074cc0e9de82d85ab33681537891fd79616300c
VijayaKumar S <vjai.s91@gmail.com>
2018-12-01 14:02:45 +0530

    Updated

--- Code/app/code/local/LWM/Freeship/Helper/Data.php
+++ Code/app/code/local/LWM/Freeship/Helper/Data.php
@@ -7,16 +7,16 @@
 
 class LWM_Freeship_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	const XML_PATH_CUSTOMER_GROUP_ENABLE			= 'carriers/lwm_freeship/active';
-	const XML_PATH_CUSTOMER_GROUP_TITLE				= 'carriers/lwm_freeship/title';
-	const XML_PATH_CUSTOMER_GROUP_NAME				= 'carriers/lwm_freeship/name';
-	const XML_PATH_CUSTOMER_GROUP_DESCRIPTION		= 'carriers/lwm_freeship/description';
-	const XML_PATH_CUSTOMER_GROUP_SUBTOTAL			= 'carriers/lwm_freeship/lwm_free_shipping_subtotal';
+	const XML_PATH_CUSTOMER_GROUP_ENABLE		= 'carriers/lwm_freeship/active';
+	const XML_PATH_CUSTOMER_GROUP_TITLE		= 'carriers/lwm_freeship/title';
+	const XML_PATH_CUSTOMER_GROUP_NAME		= 'carriers/lwm_freeship/name';
+	const XML_PATH_CUSTOMER_GROUP_DESCRIPTION	= 'carriers/lwm_freeship/description';
+	const XML_PATH_CUSTOMER_GROUP_SUBTOTAL		= 'carriers/lwm_freeship/lwm_free_shipping_subtotal';
 	const XML_PATH_CUSTOMER_GROUP_ALLOW_SPECIFIC	= 'carriers/lwm_freeship/sallowspecific';
 	const XML_PATH_CUSTOMER_GROUP_SPECIFIC_COUNTRY	= 'carriers/lwm_freeship/specificcountry';
 	const XML_PATH_CUSTOMER_GROUP_SPECIFIC_ERR_MSG	= 'carriers/lwm_freeship/specificerrmsg';
-	const XML_PATH_CUSTOMER_GROUP_LIST				= 'carriers/lwm_freeship/customergroup';
-	const XML_PATH_CUSTOMER_GROUP_SORT_ORDER		= 'carriers/lwm_freeship/sort_order';
+	const XML_PATH_CUSTOMER_GROUP_LIST		= 'carriers/lwm_freeship/customergroup';
+	const XML_PATH_CUSTOMER_GROUP_SORT_ORDER	= 'carriers/lwm_freeship/sort_order';
 
 }
 	 
