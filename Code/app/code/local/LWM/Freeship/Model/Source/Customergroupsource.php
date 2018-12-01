98270b28f140b1fa4c40b4aade0f9d863fa4dd58
VijayaKumar S <vjai.s91@gmail.com>
2018-12-01 14:01:32 +0530

    Init Commit

---
+++ Code/app/code/local/LWM/Freeship/Model/Source/Customergroupsource.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+ * @author VIJAYAKUMAR S
+ * @copyright Copyright (c) 2018
+ * @package LWM_Freeship
+ */
+ 
+class LWM_Freeship_Model_Source_Customergroupsource
+{
+    public function toOptionArray()
+    {
+		$customer_group = new Mage_Customer_Model_Group();
+		$allGroups  = $customer_group->getCollection()->toOptionHash();
+		foreach($allGroups as $key => $allGroup){
+			$customerGroup[$key]=array('value' => $key, 'label' => $allGroup);
+		}
+        return $customerGroup;
+    }
+}
